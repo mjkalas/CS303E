63c5efc25c77cce199afa841e2a2f455c4b6f681
mjkalas <minal.kalas@utexas.edu>
2017-12-06 11:25:33 -0600

    Update EasterSunday.py

--- EasterSunday.py
+++ EasterSunday.py
@@ -1,6 +1,6 @@
 #  File: EasterSunday.py
 
-#  Description: This program computes the date of Easter Sunday.
+#  Description: This program computes the date of Easter Sunday given any year.
 
 #  Student Name: Minal Kalas
 
@@ -17,32 +17,22 @@
 def main():
   #prompt the user to enter the year
   y = int (input ("Enter year: "))
-  # compute a
+  
+  #perform computations to find the day
   a = y % 19
-  # compute b
   b = y // 100
-  # compute c
   c = y % 100
-  # compute d
   d = b // 4
-  # compute e
   e = b % 4
-  # compute g
   g = (8*b+13) // 25
-  # compute h
   h = (19*a+b-d-g+15) % 30
-  # compute j
   j = c // 4
-  # compute k
   k = c % 4
-  # compute m
   m = (a+11*h) // 319
-  # compute r
   r = (2*e+2*j-k-h+32) % 7
-  # compute n
   n = (h-m+r+90) // 25
-  # compute p
   p = (h-m+r+n+19) % 32
+  
   #print the result
   month = ""
   if (n == 3):
