6e20a2f2e9acfd2bcc02d94b8f27e7cc6d4e2274
mjkalas <minal.kalas@gmail.com>
2018-05-24 11:02:57 -0500

    Updating to utilize calendar library for output

--- EasterSunday.py
+++ EasterSunday.py
@@ -1,27 +1,26 @@
-def main():
-  #prompt the user to enter the year
-  y = int (input ("Enter year: "))
-  
-  #perform computations to find the day
-  a = y % 19
-  b = y // 100
-  c = y % 100
-  d = b // 4
-  e = b % 4
-  g = (8*b+13) // 25
-  h = (19*a+b-d-g+15) % 30
-  j = c // 4
-  k = c % 4
-  m = (a+11*h) // 319
-  r = (2*e+2*j-k-h+32) % 7
-  n = (h-m+r+90) // 25
-  p = (h-m+r+n+19) % 32
-  
-  #print the result
-  month = ""
-  if (n == 3):
-    month = "March"
-  if (n == 4):
-    month = "April"
-  print ("In", y, "Easter Sunday is on", p, month, end=".")
-main()
+'''
+Assignment 1 - 2015
+CS303E
+Gives date of Easter Sunday given year using algorithm invented by Carl Friedrich Gauss
+'''
+import calendar
+
+def main ():
+    
+    y = int(input("Enter year: "))
+    a = y % 19
+    b = y // 100
+    c = y % 100
+    d = b // 4
+    e = b % 4
+    g = (8 * b + 13) // 25
+    h = (19 * a + b - d - g + 15) % 30
+    j = c // 4
+    k = c % 4
+    m = (a + 11 * h) // 319
+    r = (2 * e + 2 * j - k - h + m + 32) % 7
+    n = (h - m + r + 90) // 25
+    p = (h - m + r + n + 19) % 32
+
+    print("In", y, "Easter Sunday is on", p, calendar.month_name[n])
+main ()
