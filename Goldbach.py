631dcaa2e7f68b5870b4e0a862eddb5f3456aaf5
mjkalas <minal.kalas@gmail.com>
2018-08-13 20:29:55 -0500

    Cleans up Goldbach.py

--- Goldbach.py
+++ Goldbach.py
@@ -1,50 +1,30 @@
-# check if a number is prime
 def is_prime (n):
   limit = int (n ** 0.5) + 1
   divisor = 2
-  if (n == 1):
-	  return False
   while (divisor < limit):
     if (n % divisor == 0):
       return False
     divisor = divisor + 1
   return True
 
-# chcck if number is even
-def even(num):
-	if (num % 2 == 0):
-		return True
-	else:
-		return False
-
-#make answers print on same line
-def same_lines (n):
-	str_same = str(n)
-	i = 0
-	while (i <=(n/2-1)):
-		i = i+1
-		if (is_prime(i) and is_prime(n-i)):
-			str_same = str_same+" = "+str(i)+" + "+str(n-i)
-	return (str_same)
+def goldbach (num):
+    output = str(num)
+    i = 0
+    while (i <= (num // 2 - 1)):
+        i = i + 1
+        if (is_prime(i) and is_prime (num - i)):
+            output = output + " = " + str(i) + " + " + str(num - i)
+    return output
 
 def main ():
-	# Prompt user to enter range
-	lower = eval(input("Enter the lower limit:"))
-	upper = eval(input("Enter the upper limit:"))
-	while lower < 4:
-		print ("Lower limit must be greater than or equal to 4.")
-		lower = eval(input("Enter the lower limit:"))
-		upper = eval(input("Enter the upper limit:"))
-	while even(lower) is False or even(upper) is False:
-		print ("Lower limit and upper limit must be even numbers.")
-		lower = eval(input("Enter the lower limit:"))
-		upper = eval(input("Enter the upper limit:"))
-	while (lower > upper):
-		print ("Upper limit must be greater than lower limit.")
-		lower = eval(input("Enter the lower limit:"))
-		upper = eval(input("Enter the upper limit:"))
+    lower = int(input("Please enter the lower limit: "))
+    while ((lower < 4) or (lower % 2 != 0)):
+        lower = int(input("Please enter the lower limit: "))
+    upper = int(input("Please enter the upper limit: "))
+    while ((upper < lower) or (upper % 2 != 0)):
+        upper = int(input("Please enter the upper limit: "))
 
-	#print output
-	for i in range (lower, upper+1, 2):
-		print (same_lines(i))
-main ()
+    evens = list(range(lower, upper + 1, 2))
+    for num in evens:
+        print (goldbach (num))
+main()
